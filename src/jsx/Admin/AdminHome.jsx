2eec42c9bad057135fc8355069ed7ef237efbbe7
jangdebug <jangdebug@naver.com>
2023-11-08 14:53:55 +0900

    OF-61-관리자-상품-관리-프론트

--- src/jsx/Admin/AdminHome.jsx
+++ src/jsx/Admin/AdminHome.jsx
@@ -9,20 +9,19 @@ import PostReport from './subpages/community/PostReport';
 import CommentReport from './subpages/community/CommentReport';
 import AdminCoBuyingList from './subpages/co-buying/AdminCo-BuyingList';
 import ScrollToTop from '../ScrollToTop';
-import Footer from '../Footer';
-import AdminLogin from './AdminLogin';
 import WriteNotice from '../subpages/noticeboard/WriteNotice';
 import { useValidationAdmin } from '../../js/api/admin/ValidationAdminApi';
 
 import '../../css/common/adminCommon.css';
 import RegistProduct from './subpages/co-buying/RegistProduct';
 import DetailNotice from '../subpages/noticeboard/DetailNotice';
+import AdminCoBuyingDetail from './subpages/co-buying/AdminCoBuyingDetail';
 
 const AdminHome = () => {
     const [adminData, setAdminData] = useState();
     const [isSidebarCollapsed, setisSidebarCollapsed] = useState(true);
     const navigate = useNavigate();
-    const validationAdmin = useValidationAdmin('post', '/admin/home');
+    const validationAdmin = useValidationAdmin('post', '/admin/validate');
     useEffect(() => {
         async function validateAdmin() {
             try {
@@ -41,20 +40,21 @@ const AdminHome = () => {
         <>
             <ScrollToTop />
             <AdminHeader />
-            <div className="admin_container">
+            <div className='admin_container'>
                 <AdminSidbar isSidebarCollapsed={isSidebarCollapsed} setisSidebarCollapsed={setisSidebarCollapsed} />
-                <div className="admin_content_section_wrap">
+                <div className='admin_content_section_wrap'>
                     <Routes>
                         {/* <Route path="/user_suspended" element={<UserSuspended selectedMenu={selectedMenu} />}></Route> */}
-                        <Route path="/user_suspended" element={<UserSuspended />}></Route>
-                        <Route path="/admin_authorization" element={<AdminAuthorization />}></Route>
-                        <Route path="/admin_notice" element={<AdminNoticeList />}></Route>
-                        <Route path="/admin_notice_detail" element={<DetailNotice />}></Route>
-                        <Route path="/write_admin_notice" element={<WriteNotice />}></Route>
-                        <Route path="/post_report" element={<PostReport />}></Route>
-                        <Route path="/comment_report" element={<CommentReport />}></Route>
-                        <Route path="/co-buying_list" element={<AdminCoBuyingList />}></Route>
-                        <Route path="/regist_product" element={<RegistProduct />}></Route>
+                        <Route path='/user_suspended' element={<UserSuspended />}></Route>
+                        <Route path='/admin_authorization' element={<AdminAuthorization />}></Route>
+                        <Route path='/admin_notice' element={<AdminNoticeList />}></Route>
+                        <Route path='/admin_notice_detail' element={<DetailNotice />}></Route>
+                        <Route path='/write_admin_notice' element={<WriteNotice />}></Route>
+                        <Route path='/post_report' element={<PostReport />}></Route>
+                        <Route path='/comment_report' element={<CommentReport />}></Route>
+                        <Route path='/co-buying_list' element={<AdminCoBuyingList />}></Route>
+                        <Route path='/regist_product' element={<RegistProduct />}></Route>
+                        <Route path='/co-buying_detail' element={<AdminCoBuyingDetail />}></Route>
                     </Routes>
                 </div>
             </div>
---
+++ src/jsx/Admin/subpages/co-buying/AdminCoBuyingDetail_BackUp.jsx
@@ -0,0 +1,212 @@
+import React, { useEffect, useState } from 'react';
+import '../../../../css/subpage/cobuyingdetail.css';
+import { useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useValidationAdminItem } from '../../../../js/api/admin/ValidationAdminItem';
+import { adminStateAction } from '../../../../js/api/redux_store/slice/adminLoginSlice';
+
+const AdminCoBuyingDetail = () => {
+    const location = useLocation();
+    const detailProductNo = location.state ? location.state : null;
+    const [isLoading, setIsLoading] = useState(false);
+
+    const [detailCobuy, setDetailCobuy] = useState({});
+
+    const adminLoginDispatch = useDispatch();
+    const validateDetailProductAdmin = useValidationAdminItem();
+    useEffect(() => {
+        const listProduct = async () => {
+            try {
+                const validateDetailProductResponse = await validateDetailProductAdmin(
+                    'get',
+                    '/coBuy/admin/detailProduct/' + detailProductNo
+                );
+                const detailCobuy = validateDetailProductResponse.data.coBuyDetailProduct;
+
+                setDetailCobuy({
+                    adminCobuyNo: detailCobuy.no,
+                    adminCobuyId: detailCobuy.id,
+                    adminCobuyName: detailCobuy.name,
+                    adminCobuyContent: detailCobuy.content,
+                    adminCobuyPrice: detailCobuy.price,
+                    adminCobuyImg: detailCobuy.img,
+                    adminCobuyStart: detailCobuy.start_date,
+                    adminCobuyEnd: detailCobuy.end_date,
+                    adminCobuyMin: detailCobuy.min_num,
+                    adminCobuyState: detailCobuy.state,
+                    adminCobuyHit: detailCobuy.hit,
+                    adminCobuyOption1: detailCobuy.option1,
+                    adminCobuyOption2: detailCobuy.option2,
+                    adminCobuyOption3: detailCobuy.option3,
+                    adminCobuyOption4: detailCobuy.option4,
+                    adminCobuyOption5: detailCobuy.option5,
+                    adminCobuyReg: detailCobuy.reg_date,
+                    adminCobuyMod: detailCobuy.mod_date,
+                });
+
+                setIsLoading(true);
+            } catch (error) {
+                console.error('Error Message:', error.message);
+                console.error('Status Code:', error.response.status);
+                adminLoginDispatch(adminStateAction.setAdminState(false));
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        listProduct();
+    }, []);
+
+    return (
+        <>
+            <div className='co-buying_detail_wrap'>
+                <div className='co-buying_detail_second_wrap'>
+                    <div className='co-buying_detail_section'>
+                        <div className='co-buying_detail_left_section'>
+                            <div className='co-buying_detail_main_img'>
+                                <img
+                                    className='co-buying_detail_main_img'
+                                    src='https://cdn3.wadiz.kr/studio/images/2023/10/11/6d0e3a66-425f-4938-8397-5245946b222a.gif'
+                                    alt=''
+                                />
+                            </div>
+                            <hr />
+                            <div className='co-buying_detail_sub_section'>
+                                <div className='detail_sub_section_wrapper'>
+                                    <div className='detail_sub_section_title'>
+                                        <hr />
+                                        <p className='bold'>상세 설명</p>
+                                    </div>
+                                    <div className='deatil_sub_section_first_img'>
+                                        <img src='https://cdn.wadiz.kr/ft/images/green001/2023/1005/20231005035018605_43.jpeg/wadiz/resize/800/format/jpg/quality/80/' />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='boarder_line'></div>
+                        <div className='co-buying_detail_right_section'>
+                            <div className='co-buying_detail_right_container'>
+                                <div className='co-buying_category_container'>
+                                    <div className='co-buying_category_name_container'>
+                                        <a href='#none' className='co-buying_category_link'>
+                                            <span>산모용품</span>
+                                        </a>
+                                        <div className='co-buying_hastag_name_container'>
+                                            <a href='#none' className='co-buying_hastag_name_link'>
+                                                <span>
+                                                    <span className='co-buying_hastag_name_wrapper'>
+                                                        <div className='co-buying_hashtag_name'>
+                                                            <span className='hashtag_sharp'>#</span>
+                                                            <span>손목</span>
+                                                        </div>
+                                                    </span>
+                                                </span>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div className='co-buying_sharing_icon_container'>
+                                        <div className='co-buying_sharing_icon_box'>
+                                            <img src='/test_imgs/community_imgs/share.png' alt='' />
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr className='co-buying_line' />
+                                <div className='co-buying_short_info_container'>
+                                    <p className='co-buying_short_info_detail_1'>
+                                        중요한건 꺾이지 않는 손목! 중꺾손! 탄탄한 TPU소재로 편안한 디닥착 손목보호대
+                                    </p>
+                                    <p className='co-buying_short_info_detail_2'>
+                                        TPU손목보호대 디닥착 오픈 천 소재만으로는 불가능한 "탄탄한 지지력"과 "꺾임없는
+                                        편안함"을 느껴보세요! 고정/압박/온열 3가지 기능으로 매일매일 내 손목맞춤케어
+                                    </p>
+                                </div>
+                                <div className='co-buying_funding_info_container'>
+                                    <div className='co-buying_participants_remainingsdays_container flex'>
+                                        <div className='co-buying_participants_wrapper'>
+                                            <span className='bold'>334</span>
+                                            <span>명 참여</span>
+                                            &nbsp;
+                                        </div>
+                                        <span className='co-buying_remainingsdays_wrapper'>
+                                            <span className='bold'>5일 남음</span>
+                                        </span>
+                                    </div>
+                                    <div className='co-buying_funding_money_wrapper'>
+                                        <p className='co-buying_funding_money'>
+                                            <span className='bold'>16,901,500</span>
+                                            <span>원 달성</span>
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className='co-buying_funding_rate_container'>
+                                    <div className='co-buying_progressbar_wrappper'>
+                                        <div class='progressbar'>
+                                            <span class='progress'></span>
+                                        </div>
+                                    </div>
+                                    <div className='co-buying_funding_text'>
+                                        <b className='co-buying_funding_rate'>1,690%</b>
+                                        <span>판매됨</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='co-buying_options_container'>
+                                <div className='co-buygin_options_wrapper'>
+                                    <div className='co-buying_options_text'>
+                                        <p>옵션 선택</p>
+                                    </div>
+                                    <div className='co-buyging_options_wrapper'>
+                                        <select className='co-buying_options_select'>
+                                            <option>
+                                                --<span>&nbsp;&nbsp;</span>선택
+                                                <span>&nbsp;&nbsp;</span>
+                                                --
+                                            </option>
+                                            <option></option>
+                                            <option></option>
+                                            <option></option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='co-buying_buttons_container'>
+                                <div className='co-buying_buttons_wrapper flex'>
+                                    <div className='like_button_wrapper'>
+                                        <div className='like_button'>
+                                            <label htmlFor='checkbox' className='like_box'>
+                                                <input type='checkbox' id='checkbox' hidden />
+                                                <svg
+                                                    t='1689815540548'
+                                                    class='icon'
+                                                    viewBox='0 0 1024 1024'
+                                                    version='1.1'
+                                                    xmlns='http://www.w3.org/2000/svg'
+                                                    p-id='2271'
+                                                >
+                                                    <path
+                                                        d='M742.4 101.12A249.6 249.6 0 0 0 512 256a249.6 249.6 0 0 0-230.72-154.88C143.68 101.12 32 238.4 32 376.32c0 301.44 416 546.56 480 546.56s480-245.12 480-546.56c0-137.92-111.68-275.2-249.6-275.2z'
+                                                        fill='#231F20'
+                                                        p-id='2272'
+                                                        id='heart'
+                                                    ></path>
+                                                </svg>
+                                                <span></span>
+                                            </label>
+                                            <div className='likes_count'>1,546</div>
+                                        </div>
+                                    </div>
+                                    <div className='co-buying_botton_wrpper'>
+                                        <div className='buying_button'>
+                                            <p className='bold'>구매하기</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default AdminCoBuyingDetail;
